9f7763e3d0824c6bd355723642557128228f9740
majid1361 <39013835+majid1361@users.noreply.github.com>
2026-09-05 02:35:30 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const { user } = req.query;
 
   const users = {
-    "majid": "2030-01-01",
+    "majid": "2026-01-01",
     "tohid": "2030-01-01",
     "roja": "2030-01-01",
     "vida": "2030-01-01",
