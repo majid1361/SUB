66e66443f7b82b5402c509708c07b8b1931dbbc4
majid1361 <39013835+majid1361@users.noreply.github.com>
2026-09-05 02:14:08 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
 
   // 2. دیتابیسِ کاربرا (در آینده می‌تونی وصلش کنی به دیتابیس واقعی)
   const users = {
-    "majid": "2030-01-01",
+    "majid": "2025-01-01",
     "tohid": "2030-01-01",
     "roja": "2030-01-01",
     "vida": "2030-01-01",
