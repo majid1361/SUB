acdbdec40aa94d368b5467b93fea4c9c6a65cb22
majid1361 <39013835+majid1361@users.noreply.github.com>
2026-09-06 11:51:13 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const TELEGRAM_CHAT_ID = "6321830548";
 
   const users = {
-    "majid": "2030-01-01",
+    "majid": "2024-01-01",
     "tohid": "2030-01-01",
     "roja": "2030-01-01",
     "vida": "2030-01-01",
