deb1ddc11c6ba31d1b21cf16c22274f3e6f32442
majid1361 <39013835+majid1361@users.noreply.github.com>
2026-09-10 08:59:46 +0330

    Update index.js

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     "eti": "2026-10-07",
     "farbod-golkam": "2030-01-01",
     "behnam-eslami": "2026-10-07",
-    "user6": "2027-09-01",
+    "user": "2020-01-01",
     "user7": "2027-09-01",
     "user8": "2027-09-01",
     "user9": "2027-09-01",
